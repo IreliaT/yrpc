45548b9b86aa7e6d6f6c17b758462bb8ce24f222
Irelia Yu <yt5544@163.com>
2025-09-26 00:04:57 +0800

    增加注解，和扫描RPC注解

---
+++ rpc-common/src/main/java/com/ire/common/server/RpcServiceScanner.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright 2020-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ire.common.server;
+import com.ire.annotation.RpcService;
+import com.ire.common.ClassScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author binghe
+ * @version 1.0.0
+ * @description @RpcService注解扫描器
+ */
+public class RpcServiceScanner extends ClassScanner {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcServiceScanner.class);
+
+    /**
+     * 扫描指定包下的类，并筛选使用@RpcService注解标注的类
+     */
+    public static Map<String, Object> doScannerWithRpcServiceAnnotationFilterAndRegistryService(String scanPackage) throws Exception{
+        Map<String, Object> handlerMap = new HashMap<>();
+        List<String> classNameList = getClassNameList(scanPackage, true);
+        if (classNameList == null || classNameList.isEmpty()){
+            return handlerMap;
+        }
+        classNameList.stream().forEach((className) -> {
+            try {
+                Class<?> clazz = Class.forName(className);
+                RpcService rpcService = clazz.getAnnotation(RpcService.class);
+                if (rpcService != null){
+                    LOGGER.info("标注了@Rpcservice的实体类是 ===》》》" + clazz.getName());
+                }
+            } catch (Exception e) {
+                LOGGER.error("scan classes throws exception:", e);
+            }
+        });
+        return handlerMap;
+    }
+
+
+}
---
+++ rpc-test/rpc-test-scanner/src/main/java/com/ire/ScannerTest.java
@@ -0,0 +1,37 @@
+package com.ire;
+
+import com.ire.common.ClassScanner;
+import com.ire.common.reference.RpcReferenceScanner;
+import com.ire.common.server.RpcServiceScanner;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class ScannerTest {
+
+    @Test
+    public void testScannerClassNameList() throws Exception {
+        List<String> classNameList = ClassScanner.getClassNameList("com.ire.scanner.test", true);
+        classNameList.forEach(System.out::println);
+    }
+
+    /**
+     * 扫描io.binghe.rpc.test.scanner包下所有标注了@RpcService注解的类
+     */
+    @Test
+    public void testScannerClassNameListByRpcService() throws Exception {
+         RpcServiceScanner.doScannerWithRpcServiceAnnotationFilterAndRegistryService("com.ire.scanner.test");
+    }
+
+    /**
+     * 扫描io.binghe.rpc.test.scanner包下所有标注了@RpcReference注解的类
+     */
+    @Test
+    public void testScannerClassNameListByRpcReference() throws Exception {
+        RpcReferenceScanner.doScannerWithRpcReferenceAnnotationFilter("com.ire.scanner.test");
+    }
+}

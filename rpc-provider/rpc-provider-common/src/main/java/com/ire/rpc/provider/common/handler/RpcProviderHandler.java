aaa05f4f8f7c1c8512131310a870514d69abd13e
Irelia Yu <yutao@yoochat.co>
2025-10-23 18:23:09 +0800

    增加cglib的代理

--- rpc-provider/rpc-provider-common/src/main/java/com/ire/rpc/provider/common/handler/RpcProviderHandler.java
+++ rpc-provider/rpc-provider-common/src/main/java/com/ire/rpc/provider/common/handler/RpcProviderHandler.java
@@ -9,10 +9,13 @@ import com.ire.proeocol.enums.RpcType;
 import com.ire.proeocol.header.RpcHeader;
 import com.ire.proeocol.request.RpcRequest;
 import com.ire.proeocol.response.RpcResponse;
+import constants.RpcConstants;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import net.sf.cglib.reflect.FastClass;
+import net.sf.cglib.reflect.FastMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,9 +25,12 @@ import java.util.Map;
 public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<RpcRequest>> {
     private final Logger logger = LoggerFactory.getLogger(RpcProviderHandler.class);
 
+    private final String reflectType;
+
     private final Map<String, Object> handlerMap;
 
-    public RpcProviderHandler(Map<String, Object> handlerMap){
+    public RpcProviderHandler(String reflectType, Map<String, Object> handlerMap) {
+        this.reflectType = reflectType;
         this.handlerMap = handlerMap;
     }
 
@@ -32,7 +38,7 @@ public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<
     protected void channelRead0(ChannelHandlerContext ctx, RpcProtocol<RpcRequest> protocol) throws Exception {
         ServerThreadPool.submit(() -> {
             RpcHeader header = protocol.getHeader();
-            header.setMsgType((byte) RpcType.RESPONSE.getType());
+            header.setMsgType((byte)RpcType.RESPONSE.getType());
             RpcRequest request = protocol.getBody();
             logger.debug("Receive request " + header.getRequestId());
             RpcProtocol<RpcResponse> responseRpcProtocol = new RpcProtocol<RpcResponse>();
@@ -42,11 +48,11 @@ public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<
                 response.setResult(result);
                 response.setAsync(request.getAsync());
                 response.setOneway(request.getOneway());
-                header.setStatus((byte) RpcStatus.SUCCESS.getCode());
+                header.setStatus((byte)RpcStatus.SUCCESS.getCode());
             } catch (Throwable t) {
                 response.setError(t.toString());
-                header.setStatus((byte) RpcStatus.FAIL.getCode());
-                logger.error("RPC Server handle request error",t);
+                header.setStatus((byte)RpcStatus.FAIL.getCode());
+                logger.error("RPC Server handle request error", t);
             }
             responseRpcProtocol.setHeader(header);
             responseRpcProtocol.setBody(response);
@@ -60,10 +66,12 @@ public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<
     }
 
     private Object handle(RpcRequest request) throws Throwable {
-        String serviceKey = RpcServiceHelper.buildServiceKey(request.getClassName(), request.getVersion(), request.getGroup());
+        String serviceKey =
+            RpcServiceHelper.buildServiceKey(request.getClassName(), request.getVersion(), request.getGroup());
         Object serviceBean = handlerMap.get(serviceKey);
         if (serviceBean == null) {
-            throw new RuntimeException(String.format("service not exist: %s:%s", request.getClassName(), request.getMethodName()));
+            throw new RuntimeException(
+                String.format("service not exist: %s:%s", request.getClassName(), request.getMethodName()));
         }
 
         Class<?> serviceClass = serviceBean.getClass();
@@ -73,13 +81,13 @@ public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<
 
         logger.debug(serviceClass.getName());
         logger.debug(methodName);
-        if (parameterTypes != null && parameterTypes.length > 0){
+        if (parameterTypes != null && parameterTypes.length > 0) {
             for (int i = 0; i < parameterTypes.length; ++i) {
                 logger.debug(parameterTypes[i].getName());
             }
         }
 
-        if (parameters != null && parameters.length > 0){
+        if (parameters != null && parameters.length > 0) {
             for (int i = 0; i < parameters.length; ++i) {
                 logger.debug(parameters[i].toString());
             }
@@ -88,9 +96,31 @@ public class RpcProviderHandler extends SimpleChannelInboundHandler<RpcProtocol<
     }
 
     //TODO 目前使用JDK动态代理方式，此处埋点
-    private Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes, Object[] parameters) throws Throwable {
+    private Object invokeMethod(Object serviceBean, Class<?> serviceClass, String methodName, Class<?>[] parameterTypes,
+                                Object[] parameters) throws Throwable {
+        return switch (this.reflectType) {
+            case RpcConstants.REFLECT_TYPE_JDK ->
+                invokeJDKMethod(serviceBean, serviceClass, methodName, parameterTypes, parameters);
+
+            case RpcConstants.REFLECT_TYPE_CGLIB ->
+                invokeCGLibMethod(serviceBean, serviceClass, methodName, parameterTypes, parameters);
+
+            default -> throw new IllegalStateException("Unexpected value: " + this.reflectType);
+        };
+
+    }
+
+    public Object invokeJDKMethod(Object serviceBean, Class<?> serviceClass, String methodName,
+                                  Class<?>[] parameterTypes, Object[] parameters) throws Exception {
         Method method = serviceClass.getMethod(methodName, parameterTypes);
         method.setAccessible(true);
+        return method.invoke(serviceBean, parameters);
+    }
+
+    public Object invokeCGLibMethod(Object serviceBean, Class<?> serviceClass, String methodName,
+                                    Class<?>[] parameterTypes, Object[] parameters) throws Exception {
+        FastClass fastClass = FastClass.create(serviceClass);
+        FastMethod method = fastClass.getMethod(methodName, parameterTypes);
         return method.invoke(serviceBean, parameters);
     }
 
--- rpc-provider/rpc-provider-common/src/main/java/com/ire/rpc/provider/common/server/base/BaseServer.java
+++ rpc-provider/rpc-provider-common/src/main/java/com/ire/rpc/provider/common/server/base/BaseServer.java
@@ -29,41 +29,41 @@ public class BaseServer implements Server {
     //端口号
     protected int port = 27110;
 
-    protected Map<String,Object> handlerMap = new HashMap<>();
+    private String reflectType;
 
-    public BaseServer(String address) {
-        if (!StringUtils.isEmpty(address)){
+    protected Map<String, Object> handlerMap = new HashMap<>();
+
+    public BaseServer(String address, String reflectType) {
+        if (!StringUtils.isEmpty(address)) {
             String[] serverAddress = address.split(":");
             this.host = serverAddress[0];
             this.port = Integer.parseInt(serverAddress[1]);
         }
+        this.reflectType = reflectType;
     }
 
     @Override
     public void startNettyServer() {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-        try{
+        try {
             ServerBootstrap bootstrap = new ServerBootstrap();
             bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-                    .childHandler(new ChannelInitializer<SocketChannel>(){
-                        @Override
-                        protected void initChannel(SocketChannel channel) throws Exception {
-                            channel.pipeline()
-                                    //TODO 预留编解码，需要实现自定义协议
-                                    .addLast(new RpcDecoder())
-                                    .addLast(new RpcEncoder())
-                                    .addLast(new RpcProviderHandler(handlerMap));
-                        }
-                    })
-                    .option(ChannelOption.SO_BACKLOG,128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel channel) throws Exception {
+                        channel.pipeline()
+                            //TODO 预留编解码，需要实现自定义协议
+                            .addLast(new RpcDecoder()).addLast(new RpcEncoder())
+                            .addLast(new RpcProviderHandler(reflectType, handlerMap));
+                    }
+                }).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true);
             ChannelFuture future = bootstrap.bind(host, port).sync();
             logger.info("Server started on port {}", port);
             future.channel().closeFuture().sync();
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.error("RPC Server start error", e);
-        }finally {
+        } finally {
             workerGroup.shutdownGracefully();
             bossGroup.shutdownGracefully();
         }
